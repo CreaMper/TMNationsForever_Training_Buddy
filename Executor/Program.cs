5b5263575fae2c1c0d4128d1b50295ee24b63a92
Arkadiusz Wieruchowski <arkadiusz.wieruchowski@outlook.com>
2022-12-02 21:47:24 +0100

    Fixed issues with wrong URLs set in UID fetch

--- Executor/Program.cs
+++ Executor/Program.cs
@@ -43,20 +43,38 @@ namespace Executor
                     continue;
 
                 Logger.LogSeparator();
-                Logger.Log("Found a new map Packet!", LogTypeEnum.Info);
-                Logger.Log($"Author: {trackInfo.AuthorName} | Trackname: {trackInfo.TrackName} | UID: {trackInfo.UID}", LogTypeEnum.Info);
+                Logger.Log("Found a new map Packet!", LogTypeEnum.Success);
+                Logger.Log($"Author: {trackInfo.AuthorName} | Trackname: {Converters.TrackNameConverter(trackInfo.TrackName)} | UID: {trackInfo.UID}", LogTypeEnum.Info);
 
                 var trackIdAndSource = _factory.Request.GetTrackIdAndSource(trackInfo);
-                if (trackIdAndSource == null)
+                if (trackIdAndSource == null) 
+                {
+                    Logger.Log("Cannot find a Track ID! Buddy won't work for this map...", LogTypeEnum.CRITICAL);
                     continue;
+                }
+
+                Logger.Log("Track ID found!", LogTypeEnum.Success);
+                Logger.Log($"Track ID: {trackIdAndSource.TrackId} | Source: {trackIdAndSource.Source}", LogTypeEnum.Info);
 
                 var replayDataAndSource = _factory.Request.GetReplayId(trackIdAndSource);
                 if (replayDataAndSource == null)
+                {
+                    Logger.Log("Cannot find a replay that would be updated to TMX! Buddy won't work for this map...", LogTypeEnum.CRITICAL);
                     continue;
+                }
+
+                Logger.Log("Replay ID found!", LogTypeEnum.Success);
+                Logger.Log($"Replay by: {replayDataAndSource.Author} | Time: {replayDataAndSource.Time} | ReplayId: {replayDataAndSource.ReplayId} | Source: {replayDataAndSource.Source}", LogTypeEnum.Info);
 
                 var downloadSuccessful = _factory.Request.DownloadReplay(replayDataAndSource);
-                if (downloadSuccessful)
+                if (!downloadSuccessful)
                 {
+                    Logger.Log("Error occured when downloading a replay file! Perhaps buddy doesn't have access to file?", LogTypeEnum.CRITICAL);
+                    continue;
+                }
+                else
+                {
+                    Logger.Log("Replay downloaded successful! Injecting file to buddy client...", LogTypeEnum.Success);
                     _factory.Client.InjectReplay(_factory.Client.Buddy, replayDataAndSource);
                     continue;
                 }
